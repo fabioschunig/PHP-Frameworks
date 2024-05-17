60fce10fe444c9e9423db3f5803b2027bd4766b5
Fabio Schunig <fabio.schunig@gmail.com>
2024-05-16 23:07:31 -0300

    Saving the Chirp with Livewire

--- Laravel/chirper-livewire/resources/views/livewire/chirps/create.blade.php
+++ Laravel/chirper-livewire/resources/views/livewire/chirps/create.blade.php
@@ -1,10 +1,21 @@
 <?php
 
+use Livewire\Attributes\Validate;
 use Livewire\Volt\Component;
 
 new class extends Component
 {
+    #[Validate('required|string|max:255')]
     public string $message = '';
+
+    public function store(): void
+    {
+        $validated = $this->validate();
+
+        auth()->user()->chirps()->create($validated);
+
+        $this->message = '';
+    }
 }; ?>
 
 <div>
