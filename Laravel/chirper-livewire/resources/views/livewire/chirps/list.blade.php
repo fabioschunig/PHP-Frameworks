b31673040dfa6f3f879fc3de8c7b5758aa96ca06
Fabio Schunig <fabio.schunig@gmail.com>
2024-05-18 19:52:19 -0300

    Edit events to save and cancel Chirp edition

--- Laravel/chirper-livewire/resources/views/livewire/chirps/list.blade.php
+++ Laravel/chirper-livewire/resources/views/livewire/chirps/list.blade.php
@@ -29,6 +29,15 @@ new class extends Component
         $this->editing = $chirp;
         $this->getChirps();
     }
+
+    #[On('chirp-edit-canceled')]
+    #[On('chirp-updated')]
+    public function disableEditing(): void
+    {
+        $this->editing = null;
+
+        $this->getChirps();
+    }
 }; ?>
 
 <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
