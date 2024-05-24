63391ec6673efaf118b27593e2f4aec4de558276
Fabio Schunig <fabio.schunig@gmail.com>
2024-05-23 21:24:14 -0300

    Adding CSRF tag in create form

--- Laravel/alura-controle-series/resources/views/series/create.blade.php
+++ Laravel/alura-controle-series/resources/views/series/create.blade.php
@@ -1,5 +1,7 @@
 <x-layout title="Adicionar Série">
     <form action="/series/save" method="POST">
+        @csrf
+
         <div class="mb-3">
             <label for="nome" class="form-label">Nome:</label>
             <input type="text" id="nome" name="nome" class="form-control">
