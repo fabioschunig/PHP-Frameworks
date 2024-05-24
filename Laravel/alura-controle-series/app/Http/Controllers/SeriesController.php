e61365bd86934d1131e728c86acde28b6ee6bde9
Fabio Schunig <fabio.schunig@gmail.com>
2024-05-23 21:22:10 -0300

    Method store using DB Facade in Series Controller

--- Laravel/alura-controle-series/app/Http/Controllers/SeriesController.php
+++ Laravel/alura-controle-series/app/Http/Controllers/SeriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
@@ -20,5 +21,16 @@ class SeriesController extends Controller
     public function create()
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+
+        if (DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie])) {
+            return "OK";
+        } else {
+            return "Erro";
+        }
     }
 }
