a2946ec0a150bbf0effbbbc01a5314ab24642688
Fabio Schunig <fabio.schunig@gmail.com>
2024-05-25 21:50:11 -0300

    Grouping routes for SeriesController

--- Laravel/alura-controle-series/routes/web.php
+++ Laravel/alura-controle-series/routes/web.php
@@ -12,17 +12,23 @@ Route::get('/hello', function () {
     return "Hello, world!";
 });
 
-Route::get('/series', [
-    SeriesController::class,
-    'index',
-]);
+// Route::get('/series', [
+//     SeriesController::class,
+//     'index',
+// ]);
 
-Route::get('/series/create', [
-    SeriesController::class,
-    'create',
-]);
+// Route::get('/series/create', [
+//     SeriesController::class,
+//     'create',
+// ]);
 
-Route::post('/series/save', [
-    SeriesController::class,
-    'store',
-]);
+// Route::post('/series/save', [
+//     SeriesController::class,
+//     'store',
+// ]);
+
+Route::controller(SeriesController::class)->group(function () {
+    Route::get('/series', 'index');
+    Route::get('/series/create', 'create');
+    Route::post('/series/save', 'store');
+});
