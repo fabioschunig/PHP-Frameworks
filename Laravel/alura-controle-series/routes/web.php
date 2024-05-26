c92a81bc8feba3c31a7a90b4050fd0e79e169cb1
Fabio Schunig <fabio.schunig@gmail.com>
2024-05-25 21:46:52 -0300

    Site index redirect to Series index

--- Laravel/alura-controle-series/routes/web.php
+++ Laravel/alura-controle-series/routes/web.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect('series');
 });
 
 Route::get('/hello', function () {
