afca9eff953de5025ee630e620b218435bef8ab6
Fabio Schunig <fabio.schunig@gmail.com>
2024-05-27 21:17:21 -0300

    Changing columns NUMERO to NUMBER

--- Laravel/alura-controle-series/database/migrations/2024_05_27_234114_create_seasons_table.php
+++ Laravel/alura-controle-series/database/migrations/2024_05_27_234114_create_seasons_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('seasons', function (Blueprint $table) {
             $table->id();
-            $table->unsignedTinyInteger('numero');
+            $table->unsignedTinyInteger('number');
             $table->foreignId('series_id')->constrained();
             $table->timestamps();
         });
