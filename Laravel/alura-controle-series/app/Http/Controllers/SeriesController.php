fe5fe493eea0c7abc29071a0941a9eb4fcb609b0
Fabio Schunig <fabio.schunig@gmail.com>
2024-05-27 22:14:20 -0300

    Using arrays to reduce the number of queries

--- Laravel/alura-controle-series/app/Http/Controllers/SeriesController.php
+++ Laravel/alura-controle-series/app/Http/Controllers/SeriesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
+use App\Models\Episode;
+use App\Models\Season;
 use App\Models\Series;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -46,17 +48,33 @@ class SeriesController extends Controller
 
         $series = Series::create($request->all());
 
+        $seasons = [];
         for ($i = 1; $i <= $request->seasonsNumber; $i++) {
-            $season = $series->seasons()->create([
-                'number' => $i,
-            ]);
+            // $season = $series->seasons()->create([
+            //     'number' => $i,
+            // ]);
 
+            $seasons[] = [
+                'series_id' => $series->id,
+                'number' => $i,
+            ];
+        }
+        Season::insert($seasons);
+
+        $episodes = [];
+        foreach ($series->seasons as $season) {
             for ($j = 1; $j <= $request->episodesNumber; $j++) {
-                $season->episodes()->create([
+                // $season->episodes()->create([
+                //     'number' => $j,
+                // ]);
+
+                $episodes[] = [
+                    'season_id' => $season->id,
                     'number' => $j,
-                ]);
+                ];
             }
         }
+        Episode::insert($episodes);
 
         // session()->flash('mensagem.sucesso', "Série '{$series->name}' adicionada com sucesso");
 
