f34057321a1f923d952ec5c04109353684667dc3
Fabio Schunig <fabio.schunig@gmail.com>
2024-05-29 15:00:25 -0300

    Another way of getting the seasons list with less queries

--- Laravel/alura-controle-series/app/Http/Controllers/SeasonController.php
+++ Laravel/alura-controle-series/app/Http/Controllers/SeasonController.php
@@ -2,15 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Season;
 use App\Models\Series;
 use Illuminate\Http\Request;
 
 class SeasonController extends Controller
 {
-    public function index(Series $series)
+    // public function index(Series $series)
+    public function index(int $series)
     {
         // $seasons = $series->seasons;
-        $seasons = $series->seasons()->with('episodes')->get();
+
+        // $seasons = $series->seasons()->with('episodes')->get();
+
+        $seasons = Season::query()
+            ->with('episodes')
+            ->where('series_id', $series)
+            ->get();
 
         return view('seasons.index')
             ->with('series', $series)
