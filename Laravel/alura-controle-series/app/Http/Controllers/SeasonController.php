373bb0960c89894b232da12523b6f6a316504ff5
Fabio Schunig <fabio.schunig@gmail.com>
2024-05-29 14:51:59 -0300

    Using eager loading to get episodes count

--- Laravel/alura-controle-series/app/Http/Controllers/SeasonController.php
+++ Laravel/alura-controle-series/app/Http/Controllers/SeasonController.php
@@ -9,7 +9,8 @@ class SeasonController extends Controller
 {
     public function index(Series $series)
     {
-        $seasons = $series->seasons;
+        // $seasons = $series->seasons;
+        $seasons = $series->seasons()->with('episodes')->get();
 
         return view('seasons.index')->with('seasons', $seasons);
     }
