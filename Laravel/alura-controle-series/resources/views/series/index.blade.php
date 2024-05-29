d8210f6a0dd92712df72c9878bdb61845233030f
Fabio Schunig <fabio.schunig@gmail.com>
2024-05-29 14:46:06 -0300

    Link to series' seasons

--- Laravel/alura-controle-series/resources/views/series/index.blade.php
+++ Laravel/alura-controle-series/resources/views/series/index.blade.php
@@ -10,7 +10,9 @@
     <ul class="list-group">
         @foreach ($series as $series)
         <li class="list-group-item d-flex justify-content-between align-items-center">
-            {{ $series->name }}
+            <a href="{{ route('seasons.index', $series->id) }}">
+                {{ $series->name }}
+            </a>
 
             <span class="d-flex">
                 <a href="{{ route('series.edit', $series->id) }}" class="btn btn-secondary btn-sm">Editar</a>
