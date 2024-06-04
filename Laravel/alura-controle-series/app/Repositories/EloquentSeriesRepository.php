eb38291732f01cef9a95618a91afd601e23749b3
Fabio Schunig <fabio.schunig@gmail.com>
2024-06-03 22:05:03 -0300

    Changing SeriesRepository to an interface and implementing Eloquent repo

--- Laravel/alura-controle-series/app/Repositories/EloquentSeriesRepository.php
+++ Laravel/alura-controle-series/app/Repositories/EloquentSeriesRepository.php
@@ -2,6 +2,48 @@
 
 namespace App\Repositories;
 
-class EloquentSeriesRepository
+use App\Http\Requests\SeriesFormRequest;
+use App\Models\Series;
+use App\Models\Season;
+use App\Models\Episode;
+use Illuminate\Support\Facades\DB;
+
+class EloquentSeriesRepository implements SeriesRepository
 {
+    public function add(SeriesFormRequest $request): Series
+    {
+        return DB::transaction(function () use ($request) {
+            $series = Series::create($request->all());
+
+            $seasons = [];
+            for ($i = 1; $i <= $request->seasonsNumber; $i++) {
+                // $season = $series->seasons()->create([
+                //     'number' => $i,
+                // ]);
+
+                $seasons[] = [
+                    'series_id' => $series->id,
+                    'number' => $i,
+                ];
+            }
+            Season::insert($seasons);
+
+            $episodes = [];
+            foreach ($series->seasons as $season) {
+                for ($j = 1; $j <= $request->episodesNumber; $j++) {
+                    // $season->episodes()->create([
+                    //     'number' => $j,
+                    // ]);
+
+                    $episodes[] = [
+                        'season_id' => $season->id,
+                        'number' => $j,
+                    ];
+                }
+            }
+            Episode::insert($episodes);
+
+            return $series;
+        });
+    }
 }
