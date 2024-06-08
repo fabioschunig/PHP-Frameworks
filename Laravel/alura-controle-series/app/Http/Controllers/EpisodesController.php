b31eac38daee4b37fb8fdc64e2ac059ede2cfb8e
Fabio Schunig <fabio.schunig@gmail.com>
2024-06-07 22:29:56 -0300

    Action index for Episodes controller

--- Laravel/alura-controle-series/app/Http/Controllers/EpisodesController.php
+++ Laravel/alura-controle-series/app/Http/Controllers/EpisodesController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Season;
+
 class EpisodesController
 {
+    public function index(Season $season)
+    {
+        return view('episodes.index', ['episodes' => $season->episodes]);
+    }
 }
