dc4c307ba2b84c95fba9fcd765e26e793c23789d
Fabio Schunig <fabio.schunig@gmail.com>
2024-06-09 22:09:46 -0300

    Get the session message for alert

--- Laravel/alura-controle-series/app/Http/Controllers/EpisodesController.php
+++ Laravel/alura-controle-series/app/Http/Controllers/EpisodesController.php
@@ -11,7 +11,10 @@ class EpisodesController
 {
     public function index(Season $season)
     {
-        return view('episodes.index', ['episodes' => $season->episodes]);
+        return view('episodes.index', [
+            'episodes' => $season->episodes,
+            'mensagemSucesso' => session('mensagem.sucesso'),
+        ]);
     }
 
     public function update(Request $request, Season $season)
@@ -33,6 +36,7 @@ class EpisodesController
             DB::rollback();
         }
 
-        return to_route('episodes.index', $season->id);
+        return to_route('episodes.index', $season->id)
+            ->with('mensagem.sucesso', 'Episódios marcados com sucesso');
     }
 }
