f0d47182fb10b6548774e97ee5eac0e63f0761f7
Fabio Schunig <fabio.schunig@gmail.com>
2024-06-09 22:15:49 -0300

    Local scope Watched in Episode model

--- Laravel/alura-controle-series/app/Models/Episode.php
+++ Laravel/alura-controle-series/app/Models/Episode.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,5 +17,10 @@ class Episode extends Model
     public function season()
     {
         return $this->belongsTo(Season::class);
+    }
+
+    public function scopeWatched(Builder $query)
+    {
+        $query->where('watched', true);
     }
 }
