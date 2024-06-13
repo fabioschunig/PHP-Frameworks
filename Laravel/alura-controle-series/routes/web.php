d1d20da3e2561c0e3d585512e9b7abcc2255ba65
Fabio Schunig <fabio.schunig@gmail.com>
2024-06-12 23:37:02 -0300

    Command breeze:install

--- Laravel/alura-controle-series/routes/web.php
+++ Laravel/alura-controle-series/routes/web.php
@@ -1,78 +1,20 @@
 <?php
 
-use App\Http\Controllers\EpisodesController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\SeasonController;
-use App\Http\Controllers\SeriesController;
-use App\Http\Controllers\UserController;
-use App\Http\Middleware\Authenticator;
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return "Hello, world!";
+Route::get('/', function () {
+    return view('welcome');
 });
 
-// Route::get('/series', [
-//     SeriesController::class,
-//     'index',
-// ]);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::get('/series/create', [
-//     SeriesController::class,
-//     'create',
-// ]);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Route::post('/series/save', [
-//     SeriesController::class,
-//     'store',
-// ]);
-
-// Route::controller(SeriesController::class)->group(function () {
-//     Route::get('/series', 'index');
-//     Route::get('/series/create', 'create');
-//     Route::post('/series/save', 'store');
-// });
-
-Route::resource('/series', SeriesController::class)
-    // ->only(['index', 'create', 'store', 'destroy', 'edit', 'update']);
-    ->except(['show']);
-
-// Route::post('/series/destroy/{id}', [SeriesController::class, 'destroy'])
-//     ->name('series.destroy');
-
-//Route::middleware('autenticador')->group(function () {
-
-Route::get('/', function () {
-    // return view('welcome');
-    return redirect('series');
-})->middleware(Authenticator::class);
-
-Route::get('/series/{series}/seasons', [SeasonController::class, 'index'])
-    ->name('seasons.index')
-    ->middleware(Authenticator::class);
-
-Route::get('seasons/{season}/episodes', [EpisodesController::class, 'index'])
-    ->name('episodes.index')
-    ->middleware(Authenticator::class);
-
-Route::post('seasons/{season}/episodes',  [EpisodesController::class, 'update'])
-    ->name('episodes.update')
-    ->middleware(Authenticator::class);
-
-//});
-
-Route::get('/login', [LoginController::class, 'index'])
-    ->name('login');
-
-Route::post('/login', [LoginController::class, 'store'])
-    ->name('signin');
-
-Route::get('/logout', [LoginController::class, 'logout'])
-    ->name('logout');
-
-Route::get('/user/create', [UserController::class, 'create'])
-    ->name('user.create');
-
-Route::post('/user/create', [UserController::class, 'store'])
-    ->name('user.store');
+require __DIR__.'/auth.php';
