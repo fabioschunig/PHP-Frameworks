6f3b172fb4eac289f240892662e74ad9c57c6c67
Fabio Schunig <fabio.schunig@gmail.com>
2024-06-21 22:22:37 -0300

    Input field for cover file

--- Laravel/alura-controle-series/resources/views/series/create.blade.php
+++ Laravel/alura-controle-series/resources/views/series/create.blade.php
@@ -4,7 +4,6 @@
         @csrf
 
         <div class="row mb-3">
-
             <div class="col-8">
                 <label for="name" class="form-label">Nome:</label>
                 <input type="text" id="name" name="name" class="form-control" autofocus value="{{ old('name') }}">
@@ -19,7 +18,13 @@
                 <label for="episodesNumber" class="form-label">Nº Episódios:</label>
                 <input type="text" id="episodesNumber" name="episodesNumber" class="form-control" value="{{ old('episodesNumber') }}">
             </div>
+        </div>
 
+        <div class="row mb-3">
+            <div class="col-12">
+                <label for="cover" class="form-label">Capa</label>
+                <input type="file" id="cover" name="cover" class="form-control" accept="image/gif, image/jpeg, imagem/png">
+            </div>
         </div>
 
         <button type="submit" class="btn btn-success">Salvar</button>
