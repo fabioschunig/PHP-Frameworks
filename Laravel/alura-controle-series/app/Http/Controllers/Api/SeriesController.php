d02e1bb76ba2a4da92aea1d24320b5663e107285
Fabio Schunig <fabio.schunig@gmail.com>
2024-07-04 22:56:23 -0300

    Store Series record with repository

--- Laravel/alura-controle-series/app/Http/Controllers/Api/SeriesController.php
+++ Laravel/alura-controle-series/app/Http/Controllers/Api/SeriesController.php
@@ -5,9 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 
 class SeriesController extends Controller
 {
+    public function __construct(private SeriesRepository $seriesRepository)
+    {
+    }
+
     public function index()
     {
         return Series::all();
@@ -15,7 +20,10 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
+        // return response()
+        //     ->json(Series::create($request->all()), 201);
+
         return response()
-            ->json(Series::create($request->all()), 201);
+            ->json($this->seriesRepository->add($request), 201);
     }
 }
