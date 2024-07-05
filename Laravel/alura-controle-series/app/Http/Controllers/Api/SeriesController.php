c276257d5cf8d3f565e1f281e9dd194c0a0560fc
Fabio Schunig <fabio.schunig@gmail.com>
2024-07-04 22:36:14 -0300

    Method store in API SeriesController

--- Laravel/alura-controle-series/app/Http/Controllers/Api/SeriesController.php
+++ Laravel/alura-controle-series/app/Http/Controllers/Api/SeriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
 
 class SeriesController extends Controller
@@ -10,5 +11,10 @@ class SeriesController extends Controller
     public function index()
     {
         return Series::all();
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        dd($request->all());
     }
 }
