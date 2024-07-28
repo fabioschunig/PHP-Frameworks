0146b114f42f2fce5623902777def936faa3785a
Fabio Schunig <fabio.schunig@gmail.com>
2024-07-28 20:47:25 -0300

    Login test route for API

--- Laravel/alura-controle-series/routes/api.php
+++ Laravel/alura-controle-series/routes/api.php
@@ -39,3 +39,8 @@ Route::get('/series/{series}/episodes', [\App\Http\Controllers\Api\SeriesControl
 
 Route::patch('/episodes/{episode}', [\App\Http\Controllers\Api\EpisodeController::class, 'watched'])
     ->name('api.episode.watched');
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only(['email', 'password']);
+    dd($credentials);
+});
