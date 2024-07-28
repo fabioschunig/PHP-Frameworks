c089ca1838db8801eae177b94a19918b0088698d
Fabio Schunig <fabio.schunig@gmail.com>
2024-07-28 20:55:07 -0300

    Find User by e-mail in API login

--- Laravel/alura-controle-series/app/Http/Controllers/Api/LoginController.php
+++ Laravel/alura-controle-series/app/Http/Controllers/Api/LoginController.php
@@ -2,6 +2,18 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+
 class LoginController
 {
+    public function login(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+        //dd($credentials);
+
+        $user = User::whereEmail($credentials['email'])
+            ->first();
+        dd($user);
+    }
 }
