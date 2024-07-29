0d960cf65fea3d22b97b61b61a0abea122240afa
Fabio Schunig <fabio.schunig@gmail.com>
2024-07-28 21:08:39 -0300

    Using Sanctum to authorize API routes

--- Laravel/alura-controle-series/routes/api.php
+++ Laravel/alura-controle-series/routes/api.php
@@ -7,43 +7,48 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Route::get('/series', function () {
-//     return \App\Models\Series::all();
-// });
+Route::middleware('auth:sanctum')->group(function () {
 
-// Route::get('/series', [\App\Http\Controllers\Api\SeriesController::class, 'index'])
-//     ->name('api.series.index');
+    // Route::get('/series', function () {
+    //     return \App\Models\Series::all();
+    // });
 
-Route::apiResource('/series', \App\Http\Controllers\Api\SeriesController::class);
+    // Route::get('/series', [\App\Http\Controllers\Api\SeriesController::class, 'index'])
+    //     ->name('api.series.index');
 
-// Route::get('/series/{series}/seasons', function (\App\Models\Series $series) {
-//     return $series->seasons;
-// });
+    Route::apiResource('/series', \App\Http\Controllers\Api\SeriesController::class);
 
-Route::get('/series/{series}/seasons', [\App\Http\Controllers\Api\SeriesController::class, 'seasons'])
-    ->name('api.series.seasons');
+    // Route::get('/series/{series}/seasons', function (\App\Models\Series $series) {
+    //     return $series->seasons;
+    // });
 
-// Route::get('/series/{series}/episodes', function (\App\Models\Series $series) {
-//     return $series->episodes;
-// });
+    Route::get('/series/{series}/seasons', [\App\Http\Controllers\Api\SeriesController::class, 'seasons'])
+        ->name('api.series.seasons');
 
-Route::get('/series/{series}/episodes', [\App\Http\Controllers\Api\SeriesController::class, 'episodes'])
-    ->name('api.series.episodes');
+    // Route::get('/series/{series}/episodes', function (\App\Models\Series $series) {
+    //     return $series->episodes;
+    // });
 
-// Route::patch('/episodes/{episode}', function (\App\Models\Episode $episode, Request $request) {
-//     $episode->watched = $request->watched;
-//     $episode->save();
+    Route::get('/series/{series}/episodes', [\App\Http\Controllers\Api\SeriesController::class, 'episodes'])
+        ->name('api.series.episodes');
 
-//     return $episode;
-// });
+    // Route::patch('/episodes/{episode}', function (\App\Models\Episode $episode, Request $request) {
+    //     $episode->watched = $request->watched;
+    //     $episode->save();
 
-Route::patch('/episodes/{episode}', [\App\Http\Controllers\Api\EpisodeController::class, 'watched'])
-    ->name('api.episode.watched');
+    //     return $episode;
+    // });
 
-// Route::post('/login', function (Request $request) {
-//     $credentials = $request->only(['email', 'password']);
-//     dd($credentials);
-// });
+    Route::patch('/episodes/{episode}', [\App\Http\Controllers\Api\EpisodeController::class, 'watched'])
+        ->name('api.episode.watched');
+
+    // Route::post('/login', function (Request $request) {
+    //     $credentials = $request->only(['email', 'password']);
+    //     dd($credentials);
+    // });
+
+
+});
 
 Route::post('/login', [\App\Http\Controllers\Api\LoginController::class, 'login'])
     ->name('api.login');
